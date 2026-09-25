7e8e5ea3b728a88fb665e070bf69552882cb0faa
Muhammad Fahad <mfahad99990@gmail.com>
2026-09-25 19:54:08 +0500

    error
    error solve

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api": {
-        target: "ecommerce-stripe-ruddy.vercel.app",
+        target: "https://ecommerce-stripe-ruddy.vercel.app",
       }
     }
   }
