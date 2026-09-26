7196186804e1b538c5829fbf39f94a5add47a108
Muhammad Fahad <mfahad99990@gmail.com>
2026-09-26 13:55:13 +0500

    Add changeOrigin and secure options to proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
     proxy:{
       "/api": {
         target: "https://ecommerce-stripe-ruddy.vercel.app",
+         changeOrigin: true,
+          secure: false
       }
     }
   }
